dda9f21a2e526241e19db15fa901f6e1202907cf
Clément Nancelle <clement.nancelle@epitech.eu>
2019-11-22 11:48:17 +0100

    feat(users): adding new port and users friends

--- users-api-service/src/models/users.model.ts
+++ users-api-service/src/models/users.model.ts
@@ -3,15 +3,18 @@
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 import { Application } from '../declarations';
+import * as mongoose from 'mongoose'
+
+const ObjectId = mongoose.SchemaTypes.ObjectId;
 
 export default function (app: Application) {
   const mongooseClient = app.get('mongooseClient');
   const users = new mongooseClient.Schema({
-  
+
     email: {type: String, unique: true, lowercase: true},
     password: { type: String },
-  
-  
+    friendsId: { type: [ ObjectId ] }
+
   }, {
     timestamps: true
   });
